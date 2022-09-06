8862e6c8118d928c8a9f3ff06e3417ab603d86a3
Jose David Suarez Cardona <79332655+Davilez42@users.noreply.github.com>
2022-09-06 12:04:40 -0500

    Add files via upload

---
+++ Monitoria1-Grupo52.py
@@ -0,0 +1,89 @@
+#haga un programa que pida dos numeros
+#  y me imprima   el primer numero elevedado 
+# al segundo numero
+from cmath import sqrt
+import math
+
+
+entero =  45
+flotante = 45.67777
+texto = "jose"
+booleano = False #o True
+
+
+#x =  int(input("ingrese el primer numero:"))
+#y = int(input("ingreese el segundo numero:"))
+
+#resultado =  x**y
+
+#print("el resultado de la operacion es:" ,resultado)
+#-------------------------------Ejercicio 2
+#numer =  int(input("Ingrese el numero:"))
+#valor = numer*0.5
+#print(valor)
+
+#en una tienda necesitan una programa que calcule el valor
+#total de una compra, a este valor total se le debe
+#suma el valor del iva que es 19%
+#y esta compra tiene un descuento del 5%
+
+#leer valor
+#valor2 =  int(input("ingrese el valor de la compra:"))
+#iva =  19/100
+#valor_ConIva = valor2 + valor2*iva 
+#valor_total = valor_ConIva - valor_ConIva*0.05
+#print(valor_total)
+
+
+#print("larua "*10)
+#x = "hola"
+#print(type(x))
+
+
+#hacer un programa que me imprima 5 asteriscos seguidos de un espacio
+#* * * * * 
+print("Hola como \n estas ")
+
+
+#haga un programa que imprima
+#  esta figura dado un numero ingresado por el usuario
+'''
+*******
+*******
+*******
+*******
+'''
+#n =  int(input("ingre el valor de n"))
+''''
+print("* "*10)
+print("* "*10)
+print("* "*10)
+print("* "*10)
+'''
+'''
+a =  int(input("ingrese el valor de a"))
+b =  int(input("ingrese el valor de b"))
+c =  int(input("ingrese el valor de c"))
+x1 =  (-b + sqrt((b**2)-4*a*c) )/(2*a)
+x2 =  (-b - sqrt((b**2)-4*a*c) )/(2*a)
+
+'''
+
+
+'''
+print("EL VALOR DE X1 ES:",x1,"-----","EL VALOR DE X1 ES:",x2)
+print(math.pi)
+'''
+
+a =  int(input("ingrese el valor de a"))
+b =  int(input("ingrese el valor de b"))
+
+x = a**2 +2*a*b +b**2
+
+
+print(f" el resultado es {x}")
+
+
+
+
+
